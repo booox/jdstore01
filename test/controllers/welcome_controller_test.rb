c8ca225f9163d6c6880a95e78d3e4d592edab124
booox <booox@users.noreply.github.com>
2017-05-21 15:10:05 +0800

    fixed bootstrap error

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WelcomControllerTest < ActionDispatch::IntegrationTest
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
